25e3ca34d308e0d318537573f8ed8b4383b9507d
piethrower <piethrowerchamp@gmail.com>
2021-04-19 18:56:28 -0400

    Added status

--- script.py
+++ script.py
@@ -14,9 +14,14 @@ headers = {
     "User-Agent": "DiscordFurryBot V0.1",
 }
 
+
+status = "!~"
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(status))
 
 
 @client.event
