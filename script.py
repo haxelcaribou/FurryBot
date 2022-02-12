88ff7906f91ec1ce53008a9fd4593bcbf0612584
haxel caribou <piethrowerchamp@gmail.com>
2022-02-11 19:40:48 -0500

    Added automatic emoji reactions

--- script.py
+++ script.py
@@ -25,11 +25,14 @@ headers = {
 url_regex = re.compile(
     r"(https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)")
 
+
 status = "!~ tags"
 
-# returns true if any tag is on the denylist
+
+cache = []
 
 
+# returns true if any tag is on the blocklist
 def check_tags(tags):
     for tag in tags:
         if tag in blocklist.general_tags:
@@ -44,13 +47,13 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(user_message):
+    if user_message.author == client.user:
         return
 
-    channel = message.channel
+    channel = user_message.channel
 
-    message_content = message.clean_content.lower()
+    message_content = user_message.clean_content.lower()
 
     if message_content.startswith("!~ "):
 
@@ -59,7 +62,7 @@ async def on_message(message):
             "tags": message_content[3:]
         }
 
-        params["tags"] += " -huge_filesize -flash"
+        params["tags"] += " -huge_filesize -flash score:>=0"
 
         if not channel.is_nsfw():
             params["tags"] += " rating:s status:active tagcount:>15"
@@ -77,7 +80,11 @@ async def on_message(message):
 
         posts = response["posts"]
 
+        post_num = 1
+
         for post in posts:
+
+            post_num += 1
 
             if check_tags(post["tags"]["general"]):
                 continue
@@ -88,13 +95,29 @@ async def on_message(message):
             image_url = post["file"]["url"]
             image_description = post["description"]
 
-            if image_description and image_description != "" and len(image_description) < 500 and not url_regex.match(image_description):
+            if image_description and image_description != "" and len(image_description) < 500 and not url_regex.search(image_description):
                 await channel.send(image_description)
-            await channel.send(image_url)
+            bot_message = await channel.send(image_url)
+            await bot_message.add_reaction("⬅️")
+            await bot_message.add_reaction("➡️")
+
+            cache.append(
+                {"id": bot_message.id, "channel": bot_message.channel.id, "post_num": post_num, "posts": posts})
+            if len(cache) > 2:
+                old_post = cache.pop(0)
+                old_channel = client.get_channel(old_post["channel"])
+                old_message = await old_channel.fetch_message(old_post["id"])
+                await old_message.remove_reaction("⬅️", client.user)
+                await old_message.remove_reaction("➡️", client.user)
+
             return
 
         await channel.send("no images found")
         return
 
+
+@client.event
+async def on_reaction_add(reaction, user):
+    pass
 
 client.run(secrets.token)
