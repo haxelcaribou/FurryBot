32e3622ed2b46e81664e5f71baae8bdce6e3857d
Haxel Caribou <piethrowerchamp@gmail.com>
2023-04-15 17:04:10 -0400

    Fix spelling mistake

--- secrets_template.py
+++ secrets_template.py
@@ -1,6 +1,6 @@
 # rename file to secrets.py
 
 token = "YOUR DISCORD BOT TOKEN HERE"
-# I recommend creating a sperate account specifically for the bot
+# I recommend creating a separate account specifically for the bot
 login = "YOUR E621 USERNAME"
 api_key = "YOUR E621 API KEY"
