ef3ba488ffabafa8ac4bc918573c99cbeb43e5f8
Haxel Caribou <piethrowerchamp@gmail.com>
2023-04-20 23:54:13 -0400

    Cleanup + linting

--- script.py
+++ script.py
@@ -5,15 +5,17 @@ import asyncio
 import signal
 import sys
 import logging
+from datetime import datetime
 import requests
 import discord
 import blocklist
-from datetime import datetime
 
 # TODO:
-# graceful exit
 # tag search
 # rate limiting
+# ephemeral errors
+# all move posst buttons disabled when only one post
+# - Move to specific set buttons method
 
 
 intents = discord.Intents.default()
@@ -59,37 +61,52 @@ class ButtonRow(discord.ui.View):
     active_style = discord.ButtonStyle.blurple
     inactive_style = discord.ButtonStyle.grey
 
-    @discord.ui.button(style=inactive_style, label="First", emoji="⬅️", disabled=True, custom_id="first_button")
+    @discord.ui.button(style=inactive_style,
+                       label="First",
+                       emoji="⬅️",
+                       disabled=True,
+                       custom_id="first_button")
     async def first_image(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
         await change_image(interaction.message, to_left=True, to_end=True)
 
-    @discord.ui.button(style=inactive_style, label="Prev", emoji="◀️", disabled=True, custom_id="prev_button")
+    @discord.ui.button(style=inactive_style,
+                       label="Prev",
+                       emoji="◀️",
+                       disabled=True,
+                       custom_id="prev_button")
     async def prev_image(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
         await change_image(interaction.message, to_left=True)
 
-    @discord.ui.button(style=active_style, label="Next", emoji="▶️", disabled=False, custom_id="next_button")
+    @discord.ui.button(style=active_style,
+                       label="Next",
+                       emoji="▶️",
+                       disabled=False,
+                       custom_id="next_button")
     async def next_image(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
         await change_image(interaction.message)
 
-    @discord.ui.button(style=active_style, label="Last", emoji="➡️", disabled=False, custom_id="last_button")
+    @discord.ui.button(style=active_style,
+                       label="Last",
+                       emoji="➡️",
+                       disabled=False,
+                       custom_id="last_button")
     async def last_image(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
         await change_image(interaction.message, to_end=True)
 
-    @discord.ui.button(style=discord.ButtonStyle.red, label="Delete", disabled=False, custom_id="delete_message")
+    @discord.ui.button(style=discord.ButtonStyle.red,
+                       label="Delete",
+                       disabled=False,
+                       custom_id="delete_message")
     async def delete_message(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.message.delete()
         await interaction.response.send_message("Message deleted", ephemeral=True)
 
 
-def clamp(num, min_num, max_num):
-    return max(min(num, max_num), min_num)
-
-
-def pos_in_cache(message):
+def cache_index(message):
     i = 0
     while i < len(cache):
         if message == cache[i]["message"]:
@@ -153,7 +170,7 @@ async def on_ready():
 async def on_message_delete(message):
     if message.author != CLIENT.user:
         return
-    cache_pos = pos_in_cache(message)
+    cache_pos = cache_index(message)
     if cache_pos == -1:
         return
     cache.pop(cache_pos)
@@ -177,8 +194,8 @@ async def on_message(user_message):
 
         try:
             posts = get_posts(tags, is_nsfw)
-        except HTTP404Exception as e:
-            await channel.send(f"Error: Recieved status code: {e}")
+        except HTTP404Exception as status_code:
+            await channel.send(f"Error: Recieved status code: {status_code}")
             return
         except requests.exceptions.ConnectTimeout:
             await channel.send("Error: Request timed out")
@@ -197,6 +214,7 @@ async def on_message(user_message):
 
         embed = discord.Embed(title=tags, url=f"{BASE_URL}posts/{post['id']}")
         embed = set_embed_params(embed, post)
+        embed.set_author(name=author, icon_url=author.avatar.url)
         embed.set_footer(text=f"Post 1 of {len(posts)}")
 
         view = ButtonRow()
@@ -212,7 +230,7 @@ async def on_message(user_message):
 
 
 async def change_image(message, to_left=False, to_end=False):
-    cache_pos = pos_in_cache(message)
+    cache_pos = cache_index(message)
     if cache_pos == -1:
         return
     item = cache[cache_pos]
@@ -222,30 +240,23 @@ async def change_image(message, to_left=False, to_end=False):
     embed = item["embed"]
 
     if to_end:
-        if to_left:
-            pos = 0
-        else:
-            pos = len(posts) - 1
+        pos = 0 if to_left else len(posts) - 1
     else:
-        if to_left:
-            pos -= 1
-        else:
-            pos += 1
-        pos = clamp(pos, 0, len(posts) - 1)
+        pos = max(0, pos - 1) if to_left else min(pos + 1, len(posts) - 1)
+
+    cache[cache_pos]["pos"] = pos
 
     for button in view.children:
         button.disabled = False
 
-    cache[cache_pos]["pos"] = pos
-
     if pos == 0:
         for button in view.children:
-            if button.custom_id in ("first_button", "prev_button"):
-                button.disabled = True
+            button.disabled = button.custom_id in ("first_button",
+                                                   "prev_button")
     elif pos == len(posts) - 1:
         for button in view.children:
-            if button.custom_id in ("next_button", "last_button"):
-                button.disabled = True
+            button.disabled = button.custom_id in ("next_button",
+                                                   "last_button")
 
     post = posts[pos]
 
@@ -261,15 +272,22 @@ def set_embed_params(embed, post):
     embed.timestamp = datetime.now()
 
     embed.clear_fields()
-    embed.add_field(name="Uploaded", value=datetime.strptime(post["created_at"][:19], "%Y-%m-%dT%H:%M:%S").strftime("%B %-d %Y"))
+    embed.add_field(name="Uploaded",
+                    value=datetime
+                    .strptime(post["created_at"][:19], "%Y-%m-%dT%H:%M:%S")
+                    .strftime("%B %-d %Y"))
 
     artists = [t.removesuffix("_(artist)") for t in post["tags"]["artist"]
-               if t not in ("avoid_posting", "conditional_dnp", "epilepsy_warning", "sound_warning", "unknown_artist_signature")]
+               if t not in ("avoid_posting",
+                            "conditional_dnp",
+                            "epilepsy_warning",
+                            "sound_warning",
+                            "unknown_artist_signature")]
     if len(artists) == 1:
         embed.add_field(name="Artist", value=artists[0])
     elif len(artists) > 1:
         embed.add_field(name="Artists", value=", ".join(artists))
-    
+
     embed.add_field(name="Score", value=post["score"]["total"])
     # embed.add_field(name="Size", value=f"{post['file']['width']}x{post['file']['height']}")
 
